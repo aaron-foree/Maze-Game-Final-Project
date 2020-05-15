6c025d081a13ee81035e959b4f7a1d9e3afd605d
aaron-foree <aaron.foree@gmail.com>
2020-05-15 16:14:42 -0700

    wall collisions working

--- final_app.js
+++ final_app.js
@@ -1,50 +1,58 @@
-
-$(document).ready(() => {
-  let $target = $("#maze");
-  let $source = $("#sprite");
-});
-
-function testCollision(s, t) {
-  // This tests to see if the two boxes overlap
-  if (
-    s.position().left < t.position().left + t.width() &&
-    s.position().left + s.width() > t.position().left &&
-    s.position().top < t.position().top + t.height() &&
-    s.position().top + s.height() > t.position().top
-  ) {
-    // collision detected!
-    console.log("collision!");
-    return true;
-  }
-
-  // no collision
-  console.log("no collision");
-  return false;
-}
+/*eslint-env browser*/
+var c = document.getElementById("maze");
+var ctx = c.getContext("2d");
+var img = document.getElementById("level2");
+ctx.drawImage(img, 0, 0);
 
 var sprite = document.getElementById('sprite');
 
 var up = 0;
 var left = 0;
 
+window.onload = function () {
+    spawn();
+};
+
+function spawn() {
+    left += 15;
+    sprite.style.left = left + 'px';
+    up += 247;
+    sprite.style.top = up + 'px';
+}
 
 function anim(e) {
 
     if (e.keyCode == 83) {
-        up += 5;
+        up += 2;
         sprite.style.top = up + 'px';
     }
     if (e.keyCode == 87) {
-        up -= 5;
+        up -= 2;
         sprite.style.top = up + 'px';
     }
     if (e.keyCode == 68) {
-        left += 5;
+        left += 2;
         sprite.style.left = left + 'px';
     }
     if (e.keyCode == 65) {
-        left -= 5;
+        left -= 2;
         sprite.style.left = left + 'px';
+    }
+    var mazeWall1 = ctx.getImageData(left - 9, up - 9, 1, 1);
+    if (mazeWall1.data[2] == 105 && mazeWall1.data[1] == 18) {
+        location.reload();
+    }
+    var mazeWall2 = ctx.getImageData(left + 17, up + 17, 1, 1);
+    if (mazeWall2.data[2] == 105 && mazeWall2.data[1] == 18) {
+        location.reload();
+    }
+    var mazeWall3 = ctx.getImageData(left - 9, up + 17, 1, 1);
+    if (mazeWall3.data[2] == 105 && mazeWall3.data[1] == 18) {
+        location.reload();
+    }
+    var mazeWall4 = ctx.getImageData(left + 17, up - 9, 1, 1);
+    if (mazeWall4.data[2] == 105 && mazeWall4.data[1] == 18) {
+        location.reload();
     }
 }
 
